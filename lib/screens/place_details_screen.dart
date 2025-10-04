0c1f05346de32257d909f5660b3cc68d3c50d184
Abdelrahman Samy <99355940+Samyy01@users.noreply.github.com>
2025-10-04 23:50:06 +0300

    Add files via upload

---
+++ lib/screens/place_details_screen.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+
+class PlaceDetails extends StatelessWidget {
+  const PlaceDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 2,
+        titleSpacing: 10,
+        title: Center(
+          child: Text(
+            'Place Details',
+            style: TextStyle(
+              fontFamily: 'Arial',
+              fontWeight: FontWeight.bold,
+              fontSize: 28,
+              color: Color(0xFFA8845F),
+            ),
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+            side: BorderSide(color: const Color(0xffc39871), width: 3),
+          ),
+          color: Colors.white,
+          elevation: 2,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(16),
+                  topRight: Radius.circular(16),
+                ),
+                child: Image.network(
+                  'https://upload.wikimedia.org/wikipedia/commons/e/e3/Kheops-Pyramid.jpg',
+                  height: 200,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'The Great Pyramid',
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.brown.shade700,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+
+                    Text(
+                      'With its 146.59m in height, Kheops’ pyramid, has indeed deserved its modern-day nick-name of The Great Pyramid. Khefren’s adjacent pyramid appears to be somewhat higher, but this is only because it was built on a higher part of the Giza platform. It is, in fact, slightly over 3m “smaller”.',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.grey.shade800,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+
+                    Row(
+                      children: [
+                        Expanded(
+                          child: ElevatedButton.icon(
+                            onPressed: () {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  content: Text("Added to Favorites"),
+                                ),
+                              );
+                            },
+                            icon: const Icon(Icons.favorite_border),
+                            label: const Text("Add to Favorite"),
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Color(0xffc39871),
+                              foregroundColor: Colors.white,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              padding: const EdgeInsets.symmetric(vertical: 14),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 10),
+                        Expanded(
+                          child: ElevatedButton.icon(
+                            onPressed: () {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  content: Text("Added to Visit List"),
+                                ),
+                              );
+                            },
+                            icon: const Icon(Icons.list_alt),
+                            label: const Text("Add to Visit List"),
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Color(0xffc39871),
+                              foregroundColor: Colors.white,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              padding: const EdgeInsets.symmetric(vertical: 14),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
