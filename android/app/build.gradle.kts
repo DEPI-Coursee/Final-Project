4d8efd0dce5e33698f2e8c048ee67d8f88cd2015
Samyy01 <abdelrhmansamy5@gmail.com>
2025-12-09 23:23:40 +0200

    fixed connection snackbars issue

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
 
         // Bump minSdk to 23 to satisfy awesome_notifications plugin requirement
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
 
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
